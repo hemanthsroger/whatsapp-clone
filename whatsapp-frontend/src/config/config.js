d0b36feb43f5b5c73d3cfb262aa133518181bf35
Hemanth <hemanthsroger@gmail.com>
2020-09-13 00:28:36 +0530

    updated server url

--- whatsapp-frontend/src/config/config.js
+++ whatsapp-frontend/src/config/config.js
@@ -1,8 +1,8 @@
 import socketIOClient from "socket.io-client";
 
 export const ServerConfig = {
-  // serverUrl: "https://whatsapp-clone-hemi.herokuapp.com",
-  serverUrl: "http://localhost:9000",
+  serverUrl: "https://whatsapp-clone-hemi.herokuapp.com",
+  // serverUrl: "http://localhost:9000",
 };
 
 export const SocketConnection = socketIOClient.connect(ServerConfig.serverUrl, {
